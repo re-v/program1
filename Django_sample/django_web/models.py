d09b118114a985f62efc775360342fe8362ce7b7
re-v <459514912@qq.com>
2018-05-13 11:59:03 +0800

    main_code

---
+++ Django_sample/django_web/models.py
@@ -0,0 +1,16 @@
+from mongoengine import *
+from mongoengine import connect
+connect('ganji', host='127.0.0.1', port=27017)
+
+# ORM
+
+class ItemInfo(Document):
+    title = StringField()
+    url = StringField()
+    pub_date = StringField()
+    area = ListField(StringField())
+    cates = ListField(StringField())
+    look = StringField()
+    time = StringField()
+    price = IntField()
+    meta = {'collection':'item_infoS'}
